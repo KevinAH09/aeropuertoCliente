54680b1b5e2d48794717af8a868608fb3f07ce72
Bosco <bosco11jico100@gmail.com>
2020-10-12 18:54:01 -0600

    service y controller

---
+++ aeropuertoCliente/src/main/java/org/una/aeropuertocliente/controllers/ConectionController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.laboratorio.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import org.una.laboratorio.dto.AuthenticationRequest;
+import org.una.laboratorio.service.ConectionService;
+
+/**
+ *
+ * @author colo7
+ */
+public class ConectionController {
+
+    private final String urlstring = "http://localhost:8099/usuarios/";
+
+    public ConectionController() {
+    }
+
+    public List<Object> getAll() throws InterruptedException, ExecutionException, IOException {
+        return ConectionService.ListFromConnection(urlstring, Object.class);
+    }
+
+    public void add(Object object) throws InterruptedException, ExecutionException, IOException {
+        ConectionService.ObjectToConnection(urlstring, object);
+    }
+
+    public Object Login(String cedula, String password) throws InterruptedException, ExecutionException, IOException {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(cedula, password);
+        Object o = ConectionService.ObjectLogin(urlstring+"login", authenticationRequest);
+        return o;
+
+    }
+    public Object getId(String id)throws InterruptedException, ExecutionException, IOException {
+        return ConectionService.FromConnectionID(urlstring, id,Object.class);
+    }
+//     public Object getCedulaPassword(String cedula,String pass)throws InterruptedException, ExecutionException, IOException {
+//        return ConectionService.FromConnectionCedulaPassword(urlstring, cedula,pass,Object.class);
+//    }
+//    public Object getCedula(String cedula)throws InterruptedException, ExecutionException, IOException {
+//        return ConectionService.FromConnectionCedula(urlstring+"cedula/", cedula,Object.class);
+//    }
+//     public int Update(Object usu)throws InterruptedException, ExecutionException, IOException {
+//        return ConectionService.UpdateObjectToConnection(urlstring, usu.getId().toString(),usu);
+//    }
+    public static ConectionController getInstance() {
+        return UsuariocontrollerHolder.INSTANCE;
+    }
+
+    private static class UsuariocontrollerHolder {
+
+        private static final ConectionController INSTANCE = new ConectionController();
+    }
+}
---
+++ aeropuertoCliente/src/main/java/org/una/aeropuertocliente/services/ConectionService.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.laboratorio.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import org.una.aeropuertocliente.dtos.AuthenticationResponse;
+import org.una.aeropuertocliente.dtos.UsuariosDTO;
+import org.una.aeropuertocliente.utils.AppContext;
+
+/**
+ *
+ * @author Bosco
+ */
+public class ConectionService {
+    
+    public static <T> List<Object> ListFromConnection(String urlstring, Class<T> type) throws MalformedURLException, IOException {
+        Gson gson = new Gson();
+        Type listtype = new TypeToken<ArrayList<Object>>() {
+        }.getType();
+        URL url = new URL(urlstring);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json");
+
+        try ( BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            return gson.fromJson(response.toString(), listtype);
+
+        }
+    }
+    public static <T> Object FromConnectionID(String urlstring,String id, Class<T> type) throws MalformedURLException, IOException {
+        Gson gson = new Gson();
+        Type listtype = new TypeToken<UsuariosDTO>() {
+        }.getType();
+        urlstring = urlstring+id;
+        URL url = new URL(urlstring);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json");
+
+        try ( BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            return gson.fromJson(response.toString(), listtype);
+
+        }
+    }
+//     public static <T> List<Object> FromConnectionCedula(String urlstring,String cedula, Class<T> type) throws MalformedURLException, IOException {
+//        Gson gson = new Gson();
+//        Type listtype = new TypeToken<ArrayList<Object>>() {
+//        }.getType();
+//        urlstring = urlstring+cedula;
+//        URL url = new URL(urlstring);
+//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//        con.setRequestMethod("GET");
+//        con.setRequestProperty("Accept", "application/json");
+//
+//        try ( BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+//            StringBuilder response = new StringBuilder();
+//            String responseLine;
+//            while ((responseLine = br.readLine()) != null) {
+//                response.append(responseLine.trim());
+//            }
+//            return gson.fromJson(response.toString(), listtype);
+//
+//        }
+//    }
+    public static void ObjectToConnection(String urlstring, Object object) throws MalformedURLException, IOException {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").create();
+        
+        URL url = new URL(urlstring);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+
+        String data = gson.toJson(object);
+
+        try ( OutputStream os = con.getOutputStream()) {
+            byte[] input = data.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        try ( BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+        }
+    }
+    public static int UpdateObjectToConnection(String urlstring,String id, Object object) throws MalformedURLException, IOException {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").create();
+        urlstring = urlstring + id;
+        URL url = new URL(urlstring);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("PUT");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("Authorization", "bearer " + AppContext.getInstance().get("token"));
+        con.setDoOutput(true);
+
+        String data = gson.toJson(object);
+
+        try ( OutputStream os = con.getOutputStream()) {
+            byte[] input = data.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        if (con.getResponseCode() == 200) {
+
+            try ( BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+
+            }
+
+        }
+        return con.getResponseCode();
+    }
+    public static <T> Object ObjectLogin(String urlstring, Object object) throws MalformedURLException, IOException {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").create();
+        StringBuilder response = new StringBuilder();
+        Type type = new TypeToken<AuthenticationResponse>() {
+        }.getType();
+        URL url = new URL(urlstring);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+
+        String data = gson.toJson(object);
+
+        try ( OutputStream os = con.getOutputStream()) {
+            byte[] input = data.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        if (con.getResponseCode() == 200) {
+            try ( BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+
+                String responseLine;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                return gson.fromJson(response.toString(), type);
+            }
+        }
+        return null;
+    }
+}
+
