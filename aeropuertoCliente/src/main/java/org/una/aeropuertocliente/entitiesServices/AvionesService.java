7d0a5aabc05001bf447875921d49cdf3ff2515de
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-10-13 12:21:28 -0600

    Create AvionesService.java

---
+++ aeropuertoCliente/src/main/java/org/una/aeropuertocliente/entitiesServices/AvionesService.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aeropuertocliente.entitiesServices;
+
+import com.google.gson.reflect.TypeToken;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.una.aeropuertocliente.dtos.AvionesDTO;
+import org.una.aeropuertocliente.sharedService.Conection;
+
+/**
+ *
+ * @author colo7
+ */
+public class AvionesService {
+    public static List<AvionesDTO> allAviones(){
+        
+        List<AvionesDTO> listsDTOs = new ArrayList<>();
+        try {
+            listsDTOs = (List<AvionesDTO>) Conection.listFromConnection("AreaTrabajo/",AvionesDTO.class);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(LoginService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listsDTOs;
+    }
+    public static AvionesDTO idAvion(Long id){
+        
+        AvionesDTO dTO = new AvionesDTO();
+        try {
+            dTO = (AvionesDTO) Conection.oneConnection("AreaTrabajo/"+id,new TypeToken<AvionesDTO>() {}.getType());
+            
+        } catch (IOException ex) {
+            Logger.getLogger(LoginService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return dTO;
+    }
+    public static int createAvion(AvionesDTO update){
+        int codeResponse=0;
+        try {
+            codeResponse = Conection.createObjectToConnection("AreaTrabajo/",update);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return codeResponse;
+    }
+    public static int updateAvion(AvionesDTO create){
+        int codeResponse=0;
+        try {
+            codeResponse = Conection.updateObjectToConnection("AreaTrabajo/"+create.getId(),AvionesDTO.class);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return codeResponse;
+    }
+}
