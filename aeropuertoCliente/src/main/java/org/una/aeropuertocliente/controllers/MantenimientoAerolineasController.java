06eb006e862fa2b17da77de0738fd9027ad19f58
Gus2730 <gus2730@gmail.com>
2020-10-14 16:28:19 -0600

    Creacion de vista mantenimiento aerolinea

---
+++ aeropuertoCliente/src/main/java/org/una/aeropuertocliente/controllers/MantenimientoAerolineasController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aeropuertocliente.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author cfugu
+ */
+public class MantenimientoAerolineasController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    private void onActionGuardar(ActionEvent event) {
+        
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
