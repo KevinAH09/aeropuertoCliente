c8966b4be5befaab9f8789a2926bf97ef66bd4a7
Gus2730 <gus2730@gmail.com>
2020-10-15 16:35:54 -0600

    cambios

--- aeropuertoCliente/src/main/java/org/una/aeropuertocliente/controllers/MantenimientoUsuariosController.java
+++ aeropuertoCliente/src/main/java/org/una/aeropuertocliente/controllers/MantenimientoUsuariosController.java
@@ -5,9 +5,16 @@
  */
 package org.una.aeropuertocliente.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -16,6 +23,103 @@ import javafx.fxml.Initializable;
  */
 public class MantenimientoUsuariosController implements Initializable {
 
+    @FXML
+    private Label lblTitulo;
+    @FXML
+    private Label titulo;
+    @FXML
+    private Label lblId;
+    @FXML
+    private Label id;
+    @FXML
+    private Label lbltxtId;
+    @FXML
+    private JFXTextField txtId;
+    @FXML
+    private Label lblNombre;
+    @FXML
+    private Label nombre;
+    @FXML
+    private Label lbltxtNombre;
+    @FXML
+    private JFXTextField txtNombre;
+    @FXML
+    private Label lblCedula;
+    @FXML
+    private Label cedula;
+    @FXML
+    private Label lbltxtCedula;
+    @FXML
+    private JFXTextField txtCedula;
+    @FXML
+    private Label lblCorreo;
+    @FXML
+    private Label correo;
+    @FXML
+    private Label lbltxtCorreo;
+    @FXML
+    private JFXTextField txtCorreo;
+    @FXML
+    private Label lblcontrasena;
+    @FXML
+    private Label contrasena;
+    @FXML
+    private Label lblTxtContrasena;
+    @FXML
+    private JFXPasswordField txtResponsable;
+    @FXML
+    private Label lblEstado;
+    @FXML
+    private Label estado;
+    @FXML
+    private Label lblCmbEstado;
+    @FXML
+    private JFXComboBox<?> cmbEstado;
+    @FXML
+    private Label lblRol;
+    @FXML
+    private Label rol;
+    @FXML
+    private Label lblcmbRol;
+    @FXML
+    private JFXComboBox<?> cmbRoles;
+    @FXML
+    private Label lblbtnRol;
+    @FXML
+    private Label lblJefe;
+    @FXML
+    private Label jefe;
+    @FXML
+    private Label lblcmbJefe;
+    @FXML
+    private Label lblArea;
+    @FXML
+    private Label area;
+    @FXML
+    private Label lbltxtArea;
+    @FXML
+    private JFXTextField txtArea;
+    @FXML
+    private Label lblbtnArea;
+    @FXML
+    private JFXButton btnArea;
+    @FXML
+    private Label lblfecha;
+    @FXML
+    private Label fecha;
+    @FXML
+    private Label lbltxtFecha;
+    @FXML
+    private JFXTextField txtFecha;
+    @FXML
+    private Label lblGuardar;
+    @FXML
+    private JFXButton btnGuardar;
+    @FXML
+    private Label lblCancelar;
+    @FXML
+    private JFXButton btnCancelar;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +127,17 @@ public class MantenimientoUsuariosController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onActionAreas(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionGuardar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionCancelar(ActionEvent event) {
+    }
     
 }
