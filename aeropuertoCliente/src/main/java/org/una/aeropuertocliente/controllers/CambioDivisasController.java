fdeb64a6dffe7ff139ac1b382da23267bf6a910c
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-10-15 17:03:41 -0600

    cambioDivisas incompleto

--- aeropuertoCliente/src/main/java/org/una/aeropuertocliente/controllers/CambioDivisasController.java
+++ aeropuertoCliente/src/main/java/org/una/aeropuertocliente/controllers/CambioDivisasController.java
@@ -5,9 +5,14 @@
  */
 package org.una.aeropuertocliente.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,6 +20,49 @@ import javafx.fxml.Initializable;
  * @author colo7
  */
 public class CambioDivisasController implements Initializable {
+
+    @FXML
+    private JFXTextField txtIngresarMonto;
+    @FXML
+    private JFXComboBox<String> cbMoneda;
+    @FXML
+    private ImageView img1;
+    @FXML
+    private JFXTextField txtmonto1;
+    @FXML
+    private ImageView img2;
+    @FXML
+    private JFXTextField txtmonto2;
+    @FXML
+    private ImageView img3;
+    @FXML
+    private JFXTextField txtmonto3;
+    @FXML
+    private ImageView img4;
+    @FXML
+    private JFXTextField txtmonto4;
+    @FXML
+    private ImageView img5;
+    @FXML
+    private JFXTextField txtmonto5;
+    @FXML
+    private ImageView img6;
+    @FXML
+    private JFXTextField txtmonto6;
+    @FXML
+    private ImageView img7;
+    @FXML
+    private JFXTextField txtmonto7;
+    @FXML
+    private ImageView img8;
+    @FXML
+    private JFXTextField txtmonto8;
+    @FXML
+    private JFXButton btnSalir;
+    @FXML
+    private JFXButton btnExportarXML;
+    @FXML
+    private JFXButton btnExportarPDF;
 
     /**
      * Initializes the controller class.
