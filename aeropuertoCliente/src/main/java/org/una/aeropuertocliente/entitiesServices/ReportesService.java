27fee28868312fc605bcfc61327487f61753ceb3
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-11-02 23:32:38 -0600

    Update ReportesService.java

--- aeropuertoCliente/src/main/java/org/una/aeropuertocliente/entitiesServices/ReportesService.java
+++ aeropuertoCliente/src/main/java/org/una/aeropuertocliente/entitiesServices/ReportesService.java
@@ -22,10 +22,34 @@ import org.una.aeropuertocliente.sharedService.Conection;
  * @author colo7
  */
 public class ReportesService {
-      public static void reporteUsuarios() {
+      public static void reporteGastosMant(String ini,String fin) {
            String jasper = "";
         try {
-            jasper = Conection.oneConnectionReporte("reportes/gastosMant/fechaini/fechafin/lkj/uih");
+            jasper = Conection.oneConnectionReporte("reportes/gastosMant/fechaini/fechafin/"+ini+"/"+fin);
+            System.out.println(jasper);
+        byte[] repor = Base64.getDecoder().decode(jasper);
+            ByteArrayInputStream bta = null;
+            ObjectInputStream ois = null;
+            try {
+                bta = new ByteArrayInputStream(repor);
+                ois = new ObjectInputStream(bta);
+                JasperPrint jp = (JasperPrint) ois.readObject();
+                JasperViewer jv = new JasperViewer(jp, false);
+                jv.setTitle("Reporte vuelos");
+                jv.setVisible(true);
+                jv.show();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(LoginService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+      public static void reporteGastosMantEmpresa(String ini,String fin,String empresa) {
+           String jasper = "";
+        try {
+            jasper = Conection.oneConnectionReporte("reportes/gastosMant/fechaini/fechafin/empresa/"+ini+"/"+fin+"/"+empresa);
             System.out.println(jasper);
         byte[] repor = Base64.getDecoder().decode(jasper);
             ByteArrayInputStream bta = null;
